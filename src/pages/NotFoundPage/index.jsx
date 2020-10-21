3a93828e6bd0e72e9c813192a11c39b139d02899
DucJS <mrtranduc1994@outlook.com>
2020-10-21 20:29:25 +0700

    create notfoundpage

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "antd/lib/button";
 
 export default function NotFoundPage(props) {
-    return (
-        <div className="not-found-page">
-            <h1>404 NotFound</h1>
-        </div>
-    )
+  return (
+    <div className="not-found-page">
+      <h1>404 NotFound</h1>
+      <div>
+        <p>Sorry, we couldn't find this page. Please try again!</p>
+      </div>
+      <Button type="primary" size="large">
+        <Link to="/home">Home</Link>
+      </Button>
+    </div>
+  );
 }
