44bd95ae624d6488853278cf16d498a953e62cd0
DucJS <mrtranduc1994@outlook.com>
2020-10-21 21:43:57 +0700

    first build homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { Row, Col } from "antd";
+import { PageHeader, Button } from "antd";
+import { Typography } from "antd";
+
+const { Title } = Typography;
 
 export default function HomePage() {
-  return <div>This is homepage</div>;
+  return (
+    <div className="home-page">
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col lg={{ span: 16 }} xs={{ span: 24 }}>
+          <PageHeader
+            ghost={false}
+            title={<Title level={3}>NCOVID TRACKER</Title>}
+            extra={[
+              <Button key="2">Select field</Button>,
+              <Button key="1" type="primary">
+                More infor
+              </Button>,
+            ]}
+          >
+            <Row gutter={[8, 8]}>
+              <Col span={24}>list box infor</Col>
+              <Col span={24}>map</Col>
+            </Row>
+          </PageHeader>
+        </Col>
+        <Col lg={{ span: 8 }} xs={{ span: 24 }}>
+          <Row gutter={[8, 8]}>
+            <Col span={24}>list case country</Col>
+            <Col span={24}>Chart</Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
 }
